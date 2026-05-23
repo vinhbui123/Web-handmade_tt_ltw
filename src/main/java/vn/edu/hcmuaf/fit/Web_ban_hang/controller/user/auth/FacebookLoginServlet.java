587ee0e79bef221bc5b229c19c6eba3a0be3bb76
vinh123 <buiquoctuan402@gmail.com>
2026-05-23 14:56:29 +0700

    update FacebookCallbackServlet to test on server and localhost

--- src/main/java/vn/edu/hcmuaf/fit/Web_ban_hang/controller/user/auth/FacebookLoginServlet.java
+++ src/main/java/vn/edu/hcmuaf/fit/Web_ban_hang/controller/user/auth/FacebookLoginServlet.java
@@ -1,12 +1,12 @@
 package vn.edu.hcmuaf.fit.Web_ban_hang.controller.user.auth;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
 
 @WebServlet("/facebook-login")
 public class FacebookLoginServlet extends HttpServlet {
@@ -14,7 +14,14 @@ public class FacebookLoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String redirectUri = "http://localhost:8080/Web_ban_hang/facebook/callback";
+        // Tự động chọn redirect URI dựa trên domain hiện tại
+        String redirectUri;
+        String serverName = request.getServerName();
+        if (serverName.contains("ttltwtnkiet.id.vn")) {
+            redirectUri = "https://shophandmade.ttltwtnkiet.id.vn/facebook/callback";
+        } else {
+            redirectUri = "http://localhost:8080/Web_ban_hang/facebook/callback";
+        }
 
         String loginUrl = "https://www.facebook.com/v18.0/dialog/oauth?" +
                 "client_id=" + CLIENT_ID +
